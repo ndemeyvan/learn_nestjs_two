bfaac4efd16e775a59d8728dec20005c6cae3ca4
ndeme yvan <yvan.ndeme@papersoft.ng>
2022-01-03 16:39:37 +0100

    init report projet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { MessagesModule } from './messages/messages.module';
 //importe validation pipe
 import { ValidationPipe } from '@nestjs/common';
+//import user module
+import { UsersModule } from './users/users.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create(UsersModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
--- src/messages/dtos/create-message.dto.ts
+++
@@ -1,8 +0,0 @@
-// Le Data Transfer Object (DTO) est un objet qui représente un modèle de données.
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class CreateMessageDto {
-    @IsString()
-    @IsNotEmpty()
-    content: string;
-}
